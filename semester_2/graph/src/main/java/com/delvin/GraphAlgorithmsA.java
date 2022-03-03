7c8a02f2837287a3c6f6171ed9a167584d0460e1
delvin <delvinru@gmail.com>
2022-03-03 16:00:46 +0300

    Add stub methods

---
+++ semester_2/graph/src/main/java/com/delvin/GraphAlgorithmsA.java
@@ -0,0 +1,28 @@
+package com.delvin;
+
+public class GraphAlgorithmsA {
+    public static <T> void dfs() {
+        return;
+    }
+
+    public static <T> void bfs() {
+        return;
+    }
+
+    public static <T> void dijkstraAlgortihm() {
+        return;
+    }
+
+    public static <T> void kruskalAlgoritm() {
+        return;
+    }
+
+    public static <T> void primAlgorithm() {
+        return;
+    }
+
+    public static <T> void floydWarshallAlgorithm() {
+        return;
+    }
+
+}
